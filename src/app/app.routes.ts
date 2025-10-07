35335aa84b62676d857ae4dd9a47b62206f60b23
Duy <duypersonal512@gmail.com>
2025-10-07 17:05:55 -0500

    Configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login,
+    },
+    {
+        path: 'signup',
+        component: Login,
+    }
+];
